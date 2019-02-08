2fe845bd46b311aef926a1626b026193fd76a3a4
Herman Saksono <hsaksono@ccs.neu.edu>
2019-02-08 02:20:42 -0500

    add initial codes

---
+++ UserSetting/src/main/java/edu/neu/ccs/wellness/setting/SyncedSettingRepository.java
@@ -0,0 +1,12 @@
+package edu.neu.ccs.wellness.setting;
+
+/**
+ * Created by hermansaksono on 2/8/19.
+ */
+
+public class SyncedSettingRepository {
+
+    public SyncedSettingRepository() {
+
+    }
+}
